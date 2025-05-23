2b1fba65943cf051cd5ccf7af18b0f7ee0be6187
Abenezer Teshome <abenezerteshome7d@gmail.com>
2025-05-23 17:34:03 +0300

    Updated index route to include session routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ const authRouter = require('./auth');
 const collectionRouter = require('./collections');
 const noteRouter = require('./notes');
 const flashcardRouter = require('./flashcards');
+const sessionRouter = require('./sessions');
 
 router.get('/', (req, res) => { 
     res.json({ message: 'Welcome to StudyNest API' }); 
@@ -13,5 +14,6 @@ router.use('/auth', authRouter);
 router.use('/collections', collectionRouter);
 router.use('/notes', noteRouter);
 router.use('/flashcards', flashcardRouter);
+router.use('/sessions', sessionRouter);
 
 module.exports = router;
